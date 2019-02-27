7dd61761b3c4f997ff8bf9bdf8e2eae657e9a8e8
Heather <heathercdavies@gmail.com>
2019-02-27 10:58:45 +0000

    initial set up

---
+++ server/db/seeds.js
@@ -0,0 +1,20 @@
+use bookings;
+db.dropDatabase();
+
+db.bookings.insertMany([
+  {
+    name: "Barry Rodgers",
+    email: "Barry@hotmail.com",
+    checkedin: true
+  },
+  {
+    name: "Jamie Toodle",
+    email: "toodles@gmail.com",
+    checkedin: false
+  },
+  {
+    name: "Samantha Sarti",
+    email: "smartie@gmail.com",
+    checkedin: true
+  }
+]);
